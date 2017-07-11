71ab400b5771e0a2c4597316bd5ef03d4e46142f
Jordan Etzler <jozza24@gmail.com>
2017-07-11 12:25:20 +0930

    Updated Address Input

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -16,7 +16,7 @@ class ChargesController < ApplicationController
       :customer    => customer.id,
       :amount      => @amount,
       :description => 'Rails Stripe customer',
-      :currency    => 'usd'
+      :currency    => 'aud'
     )
 
   rescue Stripe::CardError => e
